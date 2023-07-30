5a2d8493c7ad0b9c2c4cc158a26f7d52b9afe767
Alex Figliolia <alexfigliolia@gmail.com>
2023-07-30 15:43:17 -0700

    Remove dist

--- dist/mjs/index.js
+++
@@ -1,7 +0,0 @@
-export { Logger } from "./Logger";
-export { Profiler } from "./Profiler";
-export { Middleware } from "./Middleware";
-export { EnhancedDerived } from "./EnhancedDerived";
-export { EnhancerFactory } from "./EnhancerFactory";
-export { EnhancedReadable } from "./EnhancedReadable";
-export { EnhancedWritable } from "./EnhancedWritable";
